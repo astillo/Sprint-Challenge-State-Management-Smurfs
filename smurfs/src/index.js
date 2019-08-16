65b053e1f2a3ce8bc22ef2b70811c61e8f20991a
astillo <clomes.alex@gmail.com>
2019-08-16 11:36:13 -0500

    SPRINT DONE BRING ON THE WEEKEND

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -2,5 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/App";
+import { createStore, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+import { reducer } from './reducers/'
+import thunk from 'redux-thunk'
+import 'semantic-ui-css/semantic.min.css'
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const store = createStore(reducer, applyMiddleware(thunk));
+
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById("root"));
